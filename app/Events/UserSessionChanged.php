d87b594b71af49212603cb32551adfac45f8ed0d
Bryanpercy118 <bryanpercy77@gmail.com>
2022-12-14 17:41:28 +0100

    Create Listener BroadcastUserLoginNotification

--- app/Events/UserSessionChanged.php
+++ app/Events/UserSessionChanged.php
@@ -4,24 +4,26 @@ namespace App\Events;
 
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class UserSessionChanged
+class UserSessionChanged implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    public $message;
+    public $type;
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($message,$type)
     {
         //
+        $this->message=$message;
+        $this->type=$type;
     }
 
     /**
@@ -31,6 +33,6 @@ class UserSessionChanged
      */
     public function broadcastOn()
     {
-        return new PrivateChannel('channel-name');
+        return new Channel('notifications');
     }
 }
